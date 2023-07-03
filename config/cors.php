d856898182ef4a4f6d59013d16da1f35fb49c56c
BalogunIzesan <sunday@izesan.com>
2023-07-03 05:55:31 +0100

    cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://izesan.com', 'http://izesan.com', '*'],
 
     'allowed_origins_patterns' => [],
 
