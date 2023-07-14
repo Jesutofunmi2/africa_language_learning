183499704a769ac13f77d73f1fa9d1e3b81777a5
BalogunIzesan <sunday@izesan.com>
2023-07-14 12:03:56 +0100

    puzzle hint edit

--- resources/views/pages/admin/edit-option.blade.php
+++ resources/views/pages/admin/edit-option.blade.php
@@ -63,6 +63,19 @@
                                         </span>
                                     @endif 
                                 </div>
+
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <textarea name="hint" class="form-control" placeholder="Hint"> {{  $option->option }} </textarea>
+                                        </div>
+                                        @if ($errors->has('hint'))
+                                            <span class="invalid-feedback" style="display: block;" role="alert">
+                                                <strong>{{ $errors->first('hint') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
+                                </div>
                                 
 
                                 <div class="row-12">
