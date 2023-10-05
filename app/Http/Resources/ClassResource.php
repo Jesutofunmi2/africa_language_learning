06068bae5e20d2bf2226e2241275c38e802ceb43
BalogunIzesan <sunday@izesan.com>
2023-10-05 09:10:49 +0100

    language id

--- app/Http/Resources/ClassResource.php
+++ app/Http/Resources/ClassResource.php
@@ -18,6 +18,7 @@ class ClassResource extends JsonResource
         return [
             'id' => $this->id,
             'language' => $this->languageQuery(),
+            'language_id' => $this->language_id,
             'classs_room_name' => $this->name,
             'class_arms' => ClassArmResource::collection($this->classarm)
         ];
@@ -27,4 +28,6 @@ class ClassResource extends JsonResource
     {
         return Language::query()->where('id', $this->language_id)->where('status', 1)->value('name');
     }
+
+    
 }
